a469af499539e4f3433b50ae193c1e9ba7642cc7
jigyeong <wlrud0126@gmail.com>
2025-01-20 15:28:55 +0900

    fix: 별도 서버돌리지 않고 테스트시 웹서버 기동하도록 설정

--- playwright.config.ts
+++ playwright.config.ts
@@ -76,8 +76,8 @@ export default defineConfig({
   testIgnore: '**/webrtc.spec.ts',
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
-    url: 'http://127.0.0.1:3000',
+    command: 'npm run dev',
+    port: 3000,
     reuseExistingServer: !process.env.CI,
   },
 });
